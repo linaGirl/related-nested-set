0db554a430c08549dc9a2c5f32ce451d1f8ca8ef
em <michael@joinbox.com>
2015-07-07 14:20:18 +0200

    now checking when updting if the new parent is a child of the node to update. added support for moving subtrees to another group

--- test/extension.js
+++ test/extension.js
@@ -76,7 +76,7 @@
 	var expect = function(val, cb){
 		if (typeof val === 'string') val = JSON.parse(val);
 
-		return function(err, result) { //log(getJSON(result), val, JSON.stringify(result), JSON.stringify(val));
+		return function(err, result) { //log(JSON.stringify(result));
 			try {
 				assert.deepEqual(getJSON(result), val);
 			} catch (err) {
@@ -267,7 +267,6 @@
 			});
 		});
 	});
-
 
 
 
@@ -552,6 +551,18 @@
 			});
 		});
 
+		it('setting a new parent that is achild of the node should fail', function(done) {
+			db.tree({id: 21}, '*').findOne(function(err, node) {
+				if (err) done(err);
+				else {
+					node.setParent(25).save(function(err, movedNode) {
+						if (err) done();
+						else done(new Error('should not be able to set parents that are children of the same subtree!'));
+					});
+				}
+			});
+		});
+
 		it('setting a new parent from another group should work when the group on the source was changed', function(done) {
 			db.tree({id: 24}, '*').findOne(function(err, node) {
 				if (err) done(err);
@@ -568,8 +579,54 @@
 				}
 			});
 		});
-	});
 
+
+		it('movin an entire subtree to another group', function(done) {
+			var moverNode, parentNode;
+
+			new db.tree({group: 333, name: '333-1'}).setParent().save().then(function(node) {
+				return new db.tree({group: 333, name: '333-2'}).setParent(node).save();
+			}).then(function(node) {
+				parentNode = node;
+				return new db.tree({group: 333, name: '333-3'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 333, name: '333-4'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 333, name: '333-5'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 444, name: '444-1'}).setParent().save();
+			}).then(function(node) {
+				return new db.tree({group: 444, name: '444-2'}).setParent(node).save();
+			}).then(function(node) {
+				moverNode = node;
+				return new db.tree({group: 444, name: '444-3'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 444, name: '444-4'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 444, name: '444-5'}).setParent(node).save();
+			}).then(function(node) {
+				return new db.tree({group: 444, name: '444-6'}).setParent(node).save();
+			}).then(function(node) {
+				return db.tree().loadTree(444);
+			}).then(function(tree) {
+				assert(JSON.stringify(tree) == '[{"id":31,"name":"444-1","group":444,"left":1,"right":12,"children":[{"id":32,"name":"444-2","group":444,"left":2,"right":11,"children":[{"id":33,"name":"444-3","group":444,"left":3,"right":10,"children":[{"id":34,"name":"444-4","group":444,"left":4,"right":9,"children":[{"id":35,"name":"444-5","group":444,"left":5,"right":8,"children":[{"id":36,"name":"444-6","group":444,"left":6,"right":7}]}]}]}]}]}]');
+				return db.tree().loadTree(333);
+			}).then(function(tree) {
+				assert(JSON.stringify(tree) == '[{"id":26,"name":"333-1","group":333,"left":1,"right":10,"children":[{"id":27,"name":"333-2","group":333,"left":2,"right":9,"children":[{"id":28,"name":"333-3","group":333,"left":3,"right":8,"children":[{"id":29,"name":"333-4","group":333,"left":4,"right":7,"children":[{"id":30,"name":"333-5","group":333,"left":5,"right":6}]}]}]}]}]');
+
+				moverNode.group = 333;
+				return moverNode.setParent(parentNode).save();
+			}).then(function() {
+				return db.tree().loadTree(333);
+			}).then(function(tree) {
+				assert(JSON.stringify(tree) == '[{"id":26,"name":"333-1","group":333,"left":1,"right":20,"children":[{"id":27,"name":"333-2","group":333,"left":2,"right":19,"children":[{"id":32,"name":"444-2","group":333,"left":3,"right":12,"children":[{"id":33,"name":"444-3","group":333,"left":4,"right":11,"children":[{"id":34,"name":"444-4","group":333,"left":5,"right":10,"children":[{"id":35,"name":"444-5","group":333,"left":6,"right":9,"children":[{"id":36,"name":"444-6","group":333,"left":7,"right":8}]}]}]}]},{"id":28,"name":"333-3","group":333,"left":13,"right":18,"children":[{"id":29,"name":"333-4","group":333,"left":14,"right":17,"children":[{"id":30,"name":"333-5","group":333,"left":15,"right":16}]}]}]}]}]');
+
+				return db.tree({group: ORM.in(333, 444)}).delete();
+			}).then(function() {
+				done();
+			}).catch(done);
+		});
+	});
 
 
 	describe('[Deleting]', function() {
@@ -618,7 +675,7 @@
 					tree[1].children = [];
 					tree[2].children.push({});
 
-					db.tree().syncTree(tree, expect('[{"id":20,"name":"root2","group":999,"left":1,"right":4,"children":[{"id":23,"name":"child1.2","group":999,"left":2,"right":3}]},{"id":18,"name":"root1","group":999,"left":5,"right":6},{"id":21,"name":"root3","group":999,"left":7,"right":12,"children":[{"id":25,"name":"child1.4","group":999,"left":8,"right":9},{"id":26,"name":null,"group":999,"left":10,"right":11}]}]', done));
+					db.tree().syncTree(tree, expect('[{"id":20,"name":"root2","group":999,"left":1,"right":4,"children":[{"id":23,"name":"child1.2","group":999,"left":2,"right":3}]},{"id":18,"name":"root1","group":999,"left":5,"right":6},{"id":21,"name":"root3","group":999,"left":7,"right":12,"children":[{"id":25,"name":"child1.4","group":999,"left":8,"right":9},{"id":37,"name":null,"group":999,"left":10,"right":11}]}]', done));
 				}
 			});
 		});
@@ -649,6 +706,6 @@
 				}
 				, {name: '3'}
 				, {name: '4'}
-			], expect('[{"id":27,"name":"1","group":999,"left":1,"right":2},{"id":28,"name":"2","group":999,"left":3,"right":12,"children":[{"id":29,"name":"2_1","group":999,"left":4,"right":5},{"id":30,"name":"2_2","group":999,"left":6,"right":7},{"id":31,"name":"2_3","group":999,"left":8,"right":11,"children":[{"id":32,"name":"2_3_1","group":999,"left":9,"right":10}]}]},{"id":33,"name":"3","group":999,"left":13,"right":14},{"id":34,"name":"4","group":999,"left":15,"right":16}]', done));
+			], expect('[{"id":38,"name":"1","group":999,"left":1,"right":2},{"id":39,"name":"2","group":999,"left":3,"right":12,"children":[{"id":40,"name":"2_1","group":999,"left":4,"right":5},{"id":41,"name":"2_2","group":999,"left":6,"right":7},{"id":42,"name":"2_3","group":999,"left":8,"right":11,"children":[{"id":43,"name":"2_3_1","group":999,"left":9,"right":10}]}]},{"id":44,"name":"3","group":999,"left":13,"right":14},{"id":45,"name":"4","group":999,"left":15,"right":16}]', done));
 		});
 	});
